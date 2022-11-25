1c1b44c690aadb65e2342499173569cb9935afb3
Yuma Aoki <>
2022-11-25 13:35:19 +0300

    add country pulldown to phone field

--- lib/login.dart
+++ lib/login.dart
@@ -3,9 +3,9 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl_phone_field/intl_phone_field.dart';
 
 import 'package:myapp/firebase/auth.dart';
-
 
 class LoginPage extends StatefulWidget {
   const LoginPage(this.menuId, this.facilityId);
@@ -18,6 +18,8 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   TextEditingController _phone = TextEditingController();
   TextEditingController _otp = TextEditingController();
+  String dialCode = "256";
+  String phone_number = "";
   bool login = false;
   bool proper_phone = true;
   bool proper_otp = true;
@@ -36,39 +38,61 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(title: Text('Sign in')),
       body: Center(
           child: Container(
-            width: 400,
-            height: 300,
-            child: Column(
-              children: [
-                Text('Please sign in to make an appointment'),
-                // const SizedBox(height: 8),
-                // Text('phone number'),
-                buildTextField('Phone number', _phone, Icons.phone, context),
-                login
-                    ? buildTextField('Password', _otp, Icons.timer, context)
-                    : const SizedBox(),
-                login
-                    ? buildSubmitButton('Login')
-                    : buildPhoneButton('Send Password'),
-              ],
+        width: 400,
+        height: 300,
+        child: Column(
+          children: [
+            Container(
+              child: Text('Please sign in to make an appointment'),
+              padding: EdgeInsets.all(8),
             ),
-          )),
+            // const SizedBox(height: 8),
+            // Text('phone number'),
+            IntlPhoneField(
+              decoration: const InputDecoration(
+                // TextFieldの見た目を整える
+                labelText: 'Phone Number',
+                border: OutlineInputBorder(),
+                // borderSide: BorderSide(),
+              ),
+              initialCountryCode: 'UG',
+              onCountryChanged: (country) {
+                //　国を変更したときに発火
+                dialCode = country.dialCode;
+                print(country);
+              },
+              onChanged: (phone) {
+                // 番号の入力・削除で発火
+                phone_number = phone.number;
+                print(phone);
+              },
+              invalidNumberMessage: 'Invalid Phone Number',
+            ),
+            // buildTextField('Phone number', _phone, Icons.phone, context),
+            login
+                ? buildTextField('Password', _otp, Icons.timer, context)
+                : const SizedBox(),
+            login
+                ? buildSubmitButton('Login')
+                : buildPhoneButton('Send Password'),
+          ],
+        ),
+      )),
     );
   }
 
-  Widget buildPhoneButton(String text) =>
-      ElevatedButton(
+  Widget buildPhoneButton(String text) => ElevatedButton(
         onPressed: () async {
           setState(() {
             login = !login;
           });
-          temp = await FirebaseAuthentication().sendOTP(_phone.text);
+          temp =
+              await FirebaseAuthentication().sendOTP('+$dialCode$phone_number');
         },
         child: Text(text),
       );
 
-  Widget buildSubmitButton(String text) =>
-      ElevatedButton(
+  Widget buildSubmitButton(String text) => ElevatedButton(
         onPressed: () {
           FirebaseAuthentication().authenticateMe(
               temp, _otp.text, context, widget.facilityId, widget.menuId);
@@ -76,17 +100,15 @@ class _LoginPageState extends State<LoginPage> {
         child: Text(text),
       );
 
-  Widget buildTextField(String labelText,
-      TextEditingController textEditingController,
-      IconData prefixIcons,
-      BuildContext context) =>
+  Widget buildTextField(
+          String labelText,
+          TextEditingController textEditingController,
+          IconData prefixIcons,
+          BuildContext context) =>
       Padding(
         padding: const EdgeInsets.all(10.00),
         child: SizedBox(
-          width: MediaQuery
-              .of(context)
-              .size
-              .width / 1.5,
+          width: MediaQuery.of(context).size.width / 1.5,
           child: TextFormField(
             obscureText: labelText == "OTP" ? true : false,
             controller: textEditingController,
